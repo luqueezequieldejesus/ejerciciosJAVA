b9be14c3f0ebc56ca399b375b64cd9ece416be28
Sebastián Urbina <sebaurbina@gmail.com>
2017-09-13 17:45:01 -0300

    Primer commit
    Enunciado

---
+++ EjercicioSiete.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciosiete;
+
+/**
+ *
+ * @author alumno
+ */
+public class EjercicioSiete {
+
+    /**
+     * @param args the command line arguments
+     * Se pide la base de un cuadrado y se informa el perímetro.
+     * 
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
