3f297b6d509f6614df8720387445904b5abc81dd
Sebastián Urbina <sebaurbina@gmail.com>
2017-09-15 16:07:42 -0300

    Commit 2
    Enunciado resuelto.

--- EjercicioCuatro.java
+++ EjercicioCuatro.java
@@ -5,19 +5,30 @@
  */
 package ejerciciocuatro;
 
+import java.util.Scanner;
+
 /**
  *
- * @author alumno
+ * @author sebastianurbina
  */
 public class EjercicioCuatro {
 
     /**
      * @param args the command line arguments
-     * Al ingresar una edad y el nombre se debe mostrar el siguiente mensaje: Usted se llama XXX y tiene XXX años de edad.
+     * Al ingresar la edad y el nombre mostrar el siguiente mensaje: “ Usted se llama “ XXXXXX y
+       tiene XX años de edad”.
      * 
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int edad;
+        String nombre;
+        Scanner miEscanerNuevo = new Scanner(System.in);
+        System.out.println("Ingrese su edad: ");
+        edad= miEscanerNuevo.nextInt();
+        System.out.println("Ingrese su nombre: ");
+        nombre= miEscanerNuevo.nextLine();
+        System.out.println("Usted se llama " + nombre + "y tiene " + edad + " años de edad");
     }
     
 }
