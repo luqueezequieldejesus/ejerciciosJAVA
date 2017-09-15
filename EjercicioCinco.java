6645ea5b9d7ba2014f6e4f78a73817e3327847ba
Sebastián Urbina <sebaurbina@gmail.com>
2017-09-15 16:13:50 -0300

    Commit 2
    Enunciado resuelto.

--- EjercicioCinco.java
+++ EjercicioCinco.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 
 /**
  *
- * @author alumno
+ * @author sebastianurbina
  */
 public class EjercicioCinco {
 
@@ -26,11 +26,11 @@ public class EjercicioCinco {
         Scanner miScanner = new Scanner(System.in);
         System.out.println("Ingrese el primer número: ");
         numeroUno= miScanner.nextInt();
-        System.out.println("El primer número es: " + numeroUno);
+        //System.out.println("El número " + numeroUno);
         
         System.out.println("Ingrese el segundo número: ");
         numeroDos= miScanner.nextInt();
-        System.out.println("El segundo número es: " + numeroDos);
+        //System.out.println("El número " + numeroDos);
         
         suma = numeroUno + numeroDos;
         System.out.println("La suma es " + suma);
